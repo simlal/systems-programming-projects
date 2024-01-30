765d82b2563634be75d4b9322beb04b9362f75b4
Simon Lalonde <simlalonde@hotmail.com>
2024-01-29 21:02:31 -0500

    Add remainder calc + print

--- assignements/assign2/devoir2.s
+++ assignements/assign2/devoir2.s
@@ -9,15 +9,19 @@ main:
     bl      printf
 
 inputs:
-    adr     x0, fmtLuIn
-    adr     x1, num
-    bl      scanf
-    ldr     x19, num
-    
-    adr     x0, fmtLuIn
-    adr     x1, num
-    bl      scanf
-    ldr     x20, num
+    mov     x19, 2
+    mov     x20, 10
+    mov     x21, 0
+//inputs:
+//    adr     x0, fmtLuIn
+//    adr     x1, num
+//    bl      scanf
+//    ldr     x19, num
+//    
+//    adr     x0, fmtLuIn
+//    adr     x1, num
+//    bl      scanf
+//    ldr     x20, num
 
 // Affichage des entrees (sortie)
 print_inputs:
@@ -28,18 +32,32 @@ print_inputs:
     mov     x1, x20
     bl      printf
 
+    adr     x0, fmtStrOut
+    adr     x1, msgSep
+    bl      printf
+
 // Loop entre x19 et x20 pour avoir la liste des entiers
-loop:
-    // Affichage loop pour debug
+loop_input_range:
+    mov     x22, 2    
+    udiv    x23, x19, x22    
+    msub    x23, x22, x23, x19    // remainder = (divisor*quotient) - dividend
+    
     adr     x0, fmtLuOut
     mov     x1, x19
-    bl      printf
-    // incrementation
-    cmp     x19, x20
-    b.eq    end
-    add     x19, x19, 1
-    b       loop
+    bl      printf    // print loop
+    adr     x0, fmtLuOut
+    mov     x1, x23
+    bl      printf    // print remainder
 
+    cbz     x23, skip_primecount
+    add     x21, x21, 1    // ++Total de nombre premier sinon skip
+
+skip_primecount:
+    // Incrementation et test de fin de boucle
+    add     x19, x19, 1
+    cmp     x19, x20    
+    b.eq    end
+    b       loop_input_range
 
 // "Filter" si les nombres sont premiers, compter
 
@@ -50,7 +68,7 @@ loop:
 end:
     // Fin
     adr     x0, fmtStrOut
-    adr     x1, msgInput
+    adr     x1, msgOut
     bl      printf
     mov     x0, 0
     bl      exit
@@ -64,4 +82,5 @@ fmtLuIn:        .asciz  "%lu"
 fmtLuOut:       .asciz  "%lu\n"
 fmtStrOut:      .asciz  "%s"
 msgInput:       .asciz  "Entrer 2 uint consecutivement:\n"
-messageGen:     .asciz  "Sorties\n:"
+msgSep:         .asciz  "######\n"
+msgOut:         .asciz  "### FIN! ###\n"
