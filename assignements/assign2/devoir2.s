b8685944ecc0a8c21808d0da244a2de7281fef2d
Simon Lalonde <simlalonde@hotmail.com>
2024-01-31 10:49:08 -0500

    Remove debug prints and use stdin for range

--- assignements/assign2/devoir2.s
+++ assignements/assign2/devoir2.s
@@ -1,38 +1,31 @@
 .global main
 
-// ...
 main:
+// Test sans stdin
+// non_man_inputs:
+//     ldr     x19, =5000
+//     ldr     x20, =15000
 
-// Entrees et sauvegarde des 2 unsigned int avec stdlib de C
-    adr     x0, fmtStrOut
-    adr     x1, msgInput
-    bl      printf
-
-temp_inputs:
-    mov     x19, 10
-    mov     x20, 20
-//inputs:
-//    adr     x0, fmtLuIn
-//    adr     x1, num
-//    bl      scanf
-//    ldr     x19, num
-//    
-//    adr     x0, fmtLuIn
-//    adr     x1, num
-//    bl      scanf
-//    ldr     x20, num
+// Capture des entrees de uint >= 2
+inputs:
+    adr     x0, fmtLuIn
+    adr     x1, num
+    bl      scanf
+    ldr     x19, num
+    
+    adr     x0, fmtLuIn
+    adr     x1, num
+    bl      scanf
+    ldr     x20, num
 
 // Affichage des entrees (sortie)
 print_inputs:
     adr     x0, fmtLuOut
     mov     x1, x19
     bl      printf
+    
     adr     x0, fmtLuOut
     mov     x1, x20
-    bl      printf
-
-    adr     x0, fmtStrOut
-    adr     x1, msgSep
     bl      printf
 
 initialisation:
@@ -52,50 +45,61 @@ prime_loop_start:
     // Test nb premier avec le reste de la division de x19 / innerloop (2->x19/2)
     udiv    x23, x19, x22    
     msub    x23, x22, x23, x19              // remainder = (divisor*quotient) - dividend
-    cbz     x23, input_loop_increment       // Si remainder != 0, skip au prochain int a tester
-
-    // Incremente le diviseur test premier
+    cbz     x23, input_loop_increment       // Si remainder != 0, skip au prochain int a tester 
+                                            // et pas de check palindrome
+    // Incrementation diviseur et boucle
     add     x22, x22, 1
     b       prime_loop_start
 
 // Fin de boucle premier (inner-loop)
 add_prime:
     add     x21, x21, 1                     // ++Total de nombre premier sinon skip
-    b       input_loop_increment
+    b       one_digit_palindrome            // Pas necessaire mais plus clair
 
-// Palindrome check
-//palindrome_check:
-//    cmp     x19, 10
-//    b.lt    10
+one_digit_palindrome:
+    // Palindrome check pour int entre 2-9
+    cmp     x19, 10
+    b.ge    palindrome_loop_init            // Passe
+    add     x25, x25, 1                     // Ajout total palindrome
 
-// Incrementation liste int + palindrome check + fin outer loop
+palindrome_loop_init:
+    // Skip algo palindrome si one digit
+    cmp     x19, 10
+    b.lt    input_loop_increment
+
+    // Initialisation pour renverser le nombre
+    mov     x22, 10                         // Constante pour divison et modulo b10
+    mov     x26, x19                        // Copie nb a renverser
+    mov     x27, 0                          // Conteneur nb renverse
+
+palindrome_loop:
+    udiv    x28, x26, x22                    // Extraction des chiffres poids + fort
+    msub    x26, x28, x22, x26               // Extraction du chiffre poids faible
+    mul     x27, x27, x22                    // Ajustement de la base10 en fonction de pos
+    add     x27, x27, x26                   // Somme avec iteration(s) precendente(s)
+    mov     x26, x28                        // Recule de 1 chiffre de poids fort
+    cbnz    x26, palindrome_loop            // Si 0 on sort de boucle
+
+    // Comparaison x19 et x27 (x19 inverse)
+    cmp     x19, x27
+    b.ne    input_loop_increment            // Skip si x19 != x27 apres renversement
+    add     x25, x25, 1
+
+// Incrementation et fin outer loop liste de int
 input_loop_increment:
-    adr     x0, fmtLuOut
-    mov     x1, x19
-    bl      printf
-
-    adr     x0, fmtLuOut
-    mov     x1, x24
-    bl      printf
-
     add     x19, x19, 1
-    
-    cmp     x19, x20                        // Fin outer loop liste de int
+    cmp     x19, x20                        
     b.eq    end
     b       input_loop_start
 
-// Palindrome check, compter
-
-// Redirection vers le output
-
+// Affichage des totaux et sortie
 end:
-    // Fin
-    adr     x0, fmtStrOut
-    adr     x1, msgSep
-    bl      printf
-    
     adr     x0, fmtLuOut
-    mov     x1, x21    // Prime count
+    mov     x1, x21                         // Tot premiers
+    bl      printf
+
+    adr     x0, fmtLuOut
+    mov     x1, x25                        // Premier ET palindrome
     bl      printf
 
     mov     x0, 0
@@ -108,7 +112,3 @@ num:            .skip   8
 .section ".rodata"
 fmtLuIn:        .asciz  "%lu"
 fmtLuOut:       .asciz  "%lu\n"
-fmtStrOut:      .asciz  "%s"
-msgInput:       .asciz  "Entrer 2 uint consecutivement:\n"
-msgSep:         .asciz  "######\n"
-msgOut:         .asciz  "### FIN! ###\n"
