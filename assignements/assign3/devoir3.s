ac5df8bbe9b8e4aaa80d786e27d786efd235a29f
Simon Lalonde <simlalonde@hotmail.com>
2024-02-16 12:13:58 -0500

    Add bss variables section for mem acc

--- assignements/assign3/devoir3.s
+++ assignements/assign3/devoir3.s
@@ -9,6 +9,11 @@ main:
     mov     x0, 0
     bl      exit
 
+.section ".bss"
+                .align  16
+acc             .skip   16
+mem             .skip   16*4
+
 .section ".rodata"
 fmtAcc:         .asciz  "acc:    %016lX %016lX\n"
 fmtMem:         .asciz  "mem[%lu]: %016lX %016lX\n"
