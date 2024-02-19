9f6817210f1980bc8aa2e5b5bf63622c242cef80
Simon Lalonde <simlalonde@hotmail.com>
2024-02-19 15:14:22 -0500

    Refactor init and perform acc reassign op

--- assignements/assign3/devoir3.s
+++ assignements/assign3/devoir3.s
@@ -28,69 +28,92 @@ main:
 // ###### Initialisation de acc et mem a 0 ######
 acc_init:
     // Alloc memoire 128bits avec pointeur debut x19
-    mov     x22, 0    // Constante 0 pour fill init                           
     adr     x19, acc                        
-    mov     x20, x19
     // Init a 0 aux 2 adresses
-    str     x22, [x20]                        
-    add     x20, x20, 8
-    str     x22, [x20]
-    
-    // Affichage
-    adr     x0, fmtAcc
-    mov     x1, x19
-    mov     x2, 4    // 'Index' de 4 pour diff avec mem dans ss-prog print
-    bl      print_calc_ele
+    str     xzr, [x19]                        
+    add     x19, x19, 8
+    str     xzr, [x19]
+    sub     x19, x19, 8
 
 // Alloc memoire (n=4) de 128bits avec pointeur debut x20
 mem_init:
-    mov     x22, 0    // Constante 0 pour fill init
-    mov     x23, 4    // n=4
-    mov     x24, 0    // compteur i                           
+    mov     x23, 4                      // taille mem (n)
     adr     x20, mem                         
-    mov     x21, x20    // Copie pour incrementation                         
-// Init a 0 pour chaque element de mem
-mem_loop_init:
-    cbz     x23, start_input_ops               
-    str     x22, [x21]                        
+    mov     x21, x20                    // Facilite reset *p apres sortie de boucle incrementation                         
+
+mem_loop_init:                          // Init a 0 pour chaque element de mem
+    cbz     x23, calc_loop_start               
+    str     xzr, [x21]                        
     add     x21, x21, 8
-    str     x22, [x21]
+    str     xzr, [x21]
     add     x21, x21, 8
 
-    // Appel printf
-    adr     x0, fmtMem
-    mov     x1, x20
-    mov     x2, x24
-    bl      print_calc_ele
-    
-    sub     x23, x23, 1     // Decrementation du n
-    add     x24, x24, 1     // Incrementation index i
+    sub     x23, x23, 1                 // Decrementation du n
     b       mem_loop_init
 
-// ###### Entrer dans boucle d'execution ###### // 
-start_input_ops:
-    // Chercher les entrees pour les operations
-    adr     x0, fmtOpeIn
-    adr     x1, ope
-    bl      scanf
-    ldr     x21, ope        // x21=operation1
 
+// ###### Entrer dans boucle d'execution ######
+// Utilise ss-prog: print_calc_ele(fmt, pointeurEle, index)
+
+calc_loop_start:
+// Affichage accumulateur (pAcc=x19)
+    adr     x0, fmtAcc
+    mov     x1, x19
+    mov     x2, 4                       // 'Index' de 4 pour diff avec mem dans ss-prog print
+    bl      print_calc_ele
+
+// Affichage mem (pMem=x20) avec boucle
+print_mem_init:
+    mov     x23, 4                      // n=4
+    mov     x24, 0                      // compteur i=0                           
+
+print_mem_loop:
+    cbz     x23, scan_input
+    adr     x0, fmtMem
+    mov     x1, x20
+    mov     x2, x24                     
+    bl      print_calc_ele
+    
+    sub     x23, x23, 1                 // n--
+    add     x24, x24, 1                 // i++
+    b       print_mem_loop
+
+// Chercher operateur avec entree clavier dans x21 (i)
+scan_input:
     adr     x0, fmtOpeIn
     adr     x1, ope
     bl      scanf
-    ldr     x22, ope        // x22=operation2
+    ldr     x21, ope                    // operation i=x21
 
     // Test des entrees printf
-    adr     x0, fmtOpeOut
-    mov     x1, x21
-    bl      printf
+    //adr     x0, fmtOpeOut
+    //mov     x1, x21
+    //bl      printf
 
-    adr     x0, fmtOpeOut
-    mov     x1, x22
-    bl      printf
+// ###### Logique 'switch case' pour choisir l'operation (i) a faire et entree (j) a appliquer
+
+// Operation i=0 avec effet: acc <- i
+eval_op0:
+    mov     x23, 0
+    cmp     x21, x23
+    b.ne    eval_op1
     
+    // Chercher effet 'j'
+    adr     x0, fmtOpeIn
+    adr     x1, ope
+    bl      scanf
+    ldr     x22, ope                    // effet j=x22
 
-    // Logique 'Switch case' pour comparer les 
+    // Charger la valeur de j a l'adresse pointee en memoire par acc
+    str     x22, [x19]
+    add     x19, x19, 8
+    str     xzr, [x19]                  // 0 dans 2e registre car u long 64 bits en entree
+    sub     x19, x19, 8
+
+    b       calc_loop_start
+
+eval_op1:
+
 
 end:
     mov     x0, 0
@@ -110,14 +133,14 @@ print_calc_ele:
     cmp     x22, x21
     b.lt    set_mem_printf    
     // Set les arguments de acc pour printf
-    ldr     x1, [x19]
-    ldr     x2, [x20]
+    ldr     x2, [x19]       // Registre 64bits poids fort
+    ldr     x1, [x20]       // Registre 64bits poids plus faible
     b       call_printf    
 set_mem_printf:
     // Set les arguments de mem pour printf
     mov     x1, x22
-    ldr     x2, [x19]
-    ldr     x3, [x20]
+    ldr     x3, [x19]
+    ldr     x2, [x20]
 call_printf:
     bl      printf
     
